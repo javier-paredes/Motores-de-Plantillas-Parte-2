2b4758e93b088fbcd11e21d0548b601cfd261703
Javier P <javier.aleparedes@gmail.com>
2021-06-16 01:25:05 -0300

    Corrección ejercicio pug

--- Pug/server.js
+++ Pug/server.js
@@ -22,7 +22,7 @@ const routerApi = express.Router()
 
 routerApi.get('/productos/listar', (req, res) => {
     if(productos.producto.length > 0){
-        res.render('vista', {productos: productos.producto})
+        res.render('vista', {hayProductos: true, productos: productos.producto})
     }else if (productos.producto.length == 0){
         res.render('vista', {hayProductos: false})
     }    
@@ -47,7 +47,11 @@ routerApi.post('/productos/guardar', (req, res) => {
     nuevoProducto.thumbnail = req.body.thumbnail;
     nuevoProducto.id = productos.producto.length;
     productos.guardar(nuevoProducto)   
-    res.render('vista',{productos: productos.producto})
+    if(productos.producto.length > 0){
+        res.render('vista', {hayProductos: true, productos: productos.producto})
+    }else if (productos.producto.length == 0){
+        res.render('vista', {hayProductos: false, productos: []})
+    }      
 })
 
 //ACTUALIZAR PRODUCTO POR ID
